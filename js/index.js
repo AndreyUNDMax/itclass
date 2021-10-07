3b4a552321024d1a8574180b7d4bf46433f7cacd
Maxim <maximyoutuber@yandex.ru>
2021-10-07 18:18:14 +0300

    js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,26 @@
-function Sh() {
-	document.getElementById('shop').innerHTML = 'Крепостничество это хорошо!'
+var K = 0
+function QRC() {
+	console.log('A')
+	swich(k){
+     case 1:
+     case 2:
+     case 3:
+     case 4:
+     case 5:
+     case 6:
+     case 7:
+     case 8:
+     case 9:
+     case 10:
+     case 11:
+     case 12:
+     case 13:
+     case 14:
+     break;
+     case 15:
+     a = document.getElementsById("pr__right")
+     a.style.display = ""
+     break;
+	}
+	k++
 	}
